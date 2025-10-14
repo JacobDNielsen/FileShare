965730065feb92273b1f081f1ea6f996f30cfe16
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-10-14 12:20:37 +0200

    feat: Added UserAccount model and updated dbcontext

--- src/svc/wopi-host/Data/WopiDbContext.cs
+++ src/svc/wopi-host/Data/WopiDbContext.cs
@@ -11,6 +11,7 @@ public class WopiDbContext : DbContext
 
     public DbSet<FileMetadata> Files => Set<FileMetadata>(); //vi kan i resten af koden referere til Files.NavnPåTabel
     public DbSet<FileLock> FileLocks => Set<FileLock>();
+    public DbSet<UserAccount> UserAccounts => Set<UserAccount>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -37,6 +38,19 @@ public class WopiDbContext : DbContext
              .HasForeignKey(fl => fl.FileId) //foreign key i filelock er FileId
              .HasPrincipalKey(f => f.FileId) //primary key i filmetadata er File
              .OnDelete(DeleteBehavior.Cascade); //hvis en filmetadata slettes, så slettes alle dens filelocks også
+        });
+
+        modelBuilder.Entity<UserAccount>(e =>
+        {
+            e.ToTable("useraccounts");
+            e.HasKey(ua => ua.Id);
+            e.Property(ua => ua.UserName).HasMaxLength(64).IsRequired();
+            e.Property(ua => ua.PasswordHash).HasMaxLength(256).IsRequired(); //currently plain text, but should be hashed
+            e.Property(ua => ua.Email).HasMaxLength(256).IsRequired();
+
+            e.HasIndex(ua => ua.UserName).IsUnique();
+            e.HasIndex(ua => ua.Email).IsUnique();
+
         });
     }
 }
