c9f23b20f1b39fa11d210480464a6fc2a83470be
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-10-14 16:52:38 +0200

    fix: removed redundant claim of issuer and Audience as its appended in token generation

--- src/svc/wopi-host/Services/JwtService.cs
+++ src/svc/wopi-host/Services/JwtService.cs
@@ -25,8 +25,6 @@ public sealed class JwtService : IJwtService
         var claims = new[]
         {
             new Claim(JwtRegisteredClaimNames.Sub, userId),
-            new Claim(JwtRegisteredClaimNames.Iss, _config.Issuer),
-            new Claim(JwtRegisteredClaimNames.Aud, _config.Audience),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //unique identifier for the token
             new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64), //issued at time for token
 
