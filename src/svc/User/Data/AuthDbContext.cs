7e0a26ac54f934d3ad9248327fc184bf30ace07b
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-11-04 14:00:21 +0100

    feat: adding ef core dependendies and DB to UserService

---
+++ src/svc/User/Data/AuthDbContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using User.Models;
+
+namespace User.Data;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<UserAccount> UserAccounts => Set<UserAccount>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserAccount>(e =>
+        {
+            e.ToTable("useraccounts");
+            e.HasKey(ua => ua.Id);
+            e.Property(ua => ua.UserName).HasMaxLength(64).IsRequired();
+            e.Property(ua => ua.PasswordHash).HasMaxLength(512).IsRequired();
+            e.Property(ua => ua.Email).HasMaxLength(256).IsRequired();
+
+            e.HasIndex(ua => ua.UserName).IsUnique();
+            e.HasIndex(ua => ua.Email).IsUnique();
+
+        });
+    }
+}
