44ea72d6e01893d73442f80bfedbbd44e5a2eed0
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-11-10 15:52:17 +0100

    feat: added consumer of jwt for wopi-host

--- src/svc/wopi-host/Program.cs
+++ src/svc/wopi-host/Program.cs
@@ -4,36 +4,50 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 
 var jwt = builder.Configuration.GetSection("Authentication:Jwt");
+var issuer = jwt["Issuer"]!.TrimEnd('/');
+var audience = jwt["Audience"]!.TrimEnd('/');
 
-/*builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+
+builder.Services.AddOptions<JwtConsumerConfig> ()
+    .Bind(builder.Configuration.GetSection("Authentication:Jwt"))
+    .Validate(config =>
+    {
+        return !string.IsNullOrWhiteSpace(config.Issuer) &&
+               !string.IsNullOrWhiteSpace(config.Audience);
+    }, "Invalid JWT configuration")
+    .ValidateOnStart();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    options.Authority = issuer;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
-        ValidIssuer = jwt["Issuer"],
+        ValidIssuer = issuer,
 
         ValidateAudience = true,
-        ValidAudience = jwt["Audience"],
-
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(secretKeyBytes),
+        ValidAudience = audience,
 
         ValidateLifetime = true,
         RequireExpirationTime = true,
         ClockSkew = TimeSpan.FromSeconds(30),
 
+        ValidAlgorithms = new[] { SecurityAlgorithms.RsaSha256 },
+
         //Map of OIDC-standard claim names to ASP-NET claim names
         NameClaimType = JwtRegisteredClaimNames.PreferredUsername,
         RoleClaimType = "role"
     };
-});*/
+});
 
 builder.Services.AddSwaggerGen(s =>
 {
@@ -96,7 +110,7 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
