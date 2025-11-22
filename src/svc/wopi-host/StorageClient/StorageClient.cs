9115b151cc2f354a78da237b498e2fa9ecaa332c
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-11-23 00:23:36 +0100

    fix: added jwt token injection for HTTP request from wopihost to Storage microservice

--- src/svc/wopi-host/StorageClient/StorageClient.cs
+++ src/svc/wopi-host/StorageClient/StorageClient.cs
@@ -3,11 +3,27 @@ using WopiHost.Dto;
 public sealed class StorageClient : IStorageClient
 {
     private readonly HttpClient _http;
-    public StorageClient(HttpClient http) => _http = http;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public StorageClient(HttpClient http, IHttpContextAccessor httpContextAccessor)
+    {
+        _http = http;
+        _httpContextAccessor = httpContextAccessor;
+    }
 
-    public Task<CheckFileInfoResponse?> CheckFileInfoAsync(string fileId, CancellationToken ct)
-        => _http.GetFromJsonAsync<CheckFileInfoResponse>($"/wopi/files/{Uri.EscapeDataString(fileId)}", ct);
+    public async Task<CheckFileInfoResponse?> CheckFileInfoAsync(string fileId, CancellationToken ct)
+    {
+        var authHeader = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].ToString();
+        if (!string.IsNullOrEmpty(authHeader))
+        {
+            _http.DefaultRequestHeaders.Remove("Authorization");
+            _http.DefaultRequestHeaders.Add("Authorization", authHeader);
+        }
 
+        var response =  await _http.GetAsync($"/wopi/files/{Uri.EscapeDataString(fileId)}", ct);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<CheckFileInfoResponse>(cancellationToken: ct);
+    }
     public async Task<Stream> GetFile(string fileId, CancellationToken ct)
     {
         var req = new HttpRequestMessage(HttpMethod.Get, $"/wopi/files/{Uri.EscapeDataString(fileId)}/contents");
