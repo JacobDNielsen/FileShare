0575f96b40e3373c10640cc9e9bdf2ceae22d782
MathiasHJ <mathiashj@ruc.dk>
2025-12-01 13:12:17 +0100

    change: removed userid's array from checkfile info

--- src/svc/storage/Dto/CheckFileInfoResponse.cs
+++ src/svc/storage/Dto/CheckFileInfoResponse.cs
@@ -4,7 +4,7 @@ public class CheckFileInfoResponse
     public string BaseFileName { get; set; } = null!;
     public long Size { get; set; }
     public int? OwnerId { get; set; } = null!;
-    //public int[] UserIds { get; set; } = default!;
+    public int UserId { get; set; } = default!;
     public string Version { get; set; } = null!;
     public bool UserCanWrite { get; set; } = false;
 }
