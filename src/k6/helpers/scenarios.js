0ac13b21283964bef7ab450090b17f138c64293d
JacobDNielsen <jacobpeternielsen@gmail.com>
2026-03-19 15:00:24 +0100

    feat: updated scenario with connection mode
    CONNECTION_MODE is used to determine whether TCP connection should be reused

--- src/k6/helpers/scenarios.js
+++ src/k6/helpers/scenarios.js
@@ -1,7 +1,36 @@
 import { getEnvVariable } from "./env.js";
 
+function getConnectionOptions() {
+  const connectionMode = getEnvVariable("CONNECTION_MODE");
+
+  if (!connectionMode) {
+    return {};
+  }
+
+  switch (connectionMode) {
+    case "no-reuse":
+      return {
+        noConnectionReuse: true,
+      };
+
+    case "no-vu-reuse":
+      return {
+        noVUConnectionReuse: true,
+      };
+
+    default:
+      throw new Error(
+        `Invalid CONNECTION_MODE '${connectionMode}'. Provide either "no-reuse" or "no-vu-reuse"`,
+      );
+  }
+}
+
 export function scenarioOption(scenarioName) {
   const scenarios = {
+    smoke: {
+      vus: 1,
+      iterations: 20,
+    },
     stress: {
       stages: [
         { duration: "15s", target: 10 },
@@ -42,11 +71,13 @@ export function scenarioOption(scenarioName) {
 
   return {
     insecureSkipTLSVerify: INSECURE_SKIP_TLS_VERIFY,
-    stages: picked.stages,
+    ...getConnectionOptions(),
+    ...picked,
     thresholds: {
       http_req_failed: ["rate<0.02"], // transport errors
       checks: ["rate>0.98"], // % of checks that must pass
       http_req_duration: ["p(95)<1000"],
     },
+    summaryTrendStats: ["avg", "min", "med", "max", "p(90)", "p(95)", "p(99)"],
   };
 }
